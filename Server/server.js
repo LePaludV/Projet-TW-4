11290a36004d6c75476e77935f200a844dfd2f1f
yoshibox <meddacisohan@gmail.com>
2022-03-22 15:35:26 +0100

    Ajout route pour avoir tout les endroits

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://Projet-TW-4:DkTLkWo7BXmN1Ua@cluster0.lbkjq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Projet-TW-4:DkTLkWo7BXmN1Ua@cluster0.lbkjq.mongodb.net/TW4?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect();
@@ -14,9 +14,18 @@ app.get("/test", async (req, res) => {
 
   db = client.db("TW4");
   collec = db.collection("test");
-  collec.insertMany([{"a": 1, "b": 2}]).then((ins) => {
+  collec.insertMany([{"a": 3, "b": 4}]).then((ins) => {
     console.log(ins);
   });
+});
+
+app.get("/listPlaces", async(req, res) => {
+  db = client.db("TW4");
+  collec = db.collection("places");
+
+  places = await collec.find({}).toArray();
+
+  res.json(places);
 });
 
 http.listen(3000, () => {
