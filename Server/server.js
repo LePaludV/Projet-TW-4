0b88f3a9cf64ef7cbc1a319a18de47789f1accb0
yoshibox <meddacisohan@gmail.com>
2022-03-22 14:06:42 +0100

    Base de donnée

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express();
 const http = require("http").createServer(app);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.get("/test", (req, res) => {
-	res.json({"ca marche": "oui"});
+const uri = "mongodb+srv://Projet-TW-4:DkTLkWo7BXmN1Ua@cluster0.lbkjq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect();
+
+
+app.get("/test", async (req, res) => {
+  res.json({"ca marche": "oui"});
+
+  db = client.db("TW4");
+  collec = db.collection("test");
+  collec.insertMany([{"a": 1, "b": 2}]).then((ins) => {
+    console.log(ins);
+  });
 });
 
 http.listen(3000, () => {
-	console.log("listening on *:3000");
+  console.log("listening on *:3000");
 });
