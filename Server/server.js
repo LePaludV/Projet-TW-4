4b0844d4b5943e4a1b685fca34235d1e9749ce70
LePaludV <valentin.lepalud@gmail.com>
2022-04-05 16:29:23 +0200

    Fix

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express();
-const http = require("http").createServer(app);
+const http = require("http")
+const server = http.createServer(app);
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const io = require('socket.io')(http);
-
+const io = require('socket.io')(server);
+app.use(express.json());
+var polyline = require("@mapbox/polyline");
 var players = new Set()
 
 app.get("/", (req, res) => {
@@ -15,6 +17,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect();
 
+-function httpRequest(URL, callback) {
+	http.get(URL, resp => {
+	  data = "";
+  
+	  resp.on("data", chunk => {
+		data += chunk
+	  });
+  
+	  resp.on("end", () => {
+		callback(data);
+	  });
+	});
+  }
+  
+  function password() {
+	/*alpha = "abcdefghijklmnopqrstuvwxyz";
+	elements = alpha.split("").concat(alpha.toUpperCase().split(""));*/
+	elements = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=".split("");
+	console.log(elements);
+  }
+  //password();
+  
+  function generateUserToken() {
+	setTimeout(() => {
+	  /*db = client.db("TW4");
+	  collec = db.collection("user");
+	  collec.find({}).toArray().then(res => console.log(res));*/
+	  randomBytes(4, (err, buf) => console.log(buf.toString('hex')));
+	}, 2000);
+  }
+  //generateUserToken();
+  
 
 app.get("/test", async (req, res) => {
   res.json({"ca marche": "oui"});
@@ -35,7 +69,56 @@ app.get("/listPlaces", async(req, res) => {
   res.json(places);
 });
 
-http.listen(3001, () => {
+-app.post("/create", (req, res) => {
+	username = req.body["name"];
+	token = generateUserToken();
+  
+	db = client.db("TW4");
+	collec = db.collection("user");
+	collec.insertMany({"username": username, "token": token, "trips": []}).then(ins => {
+	  console.log(ins);
+	});
+  })
+  
+  app.post("/getRoute", (req, res) => {
+	empl1 = req.body[0];
+	empl2 = req.body[1];
+	console.log(empl1, empl2);
+  
+	http.get(`http://router.project-osrm.org/route/v1/foot/${empl1[0]},${empl1[1]};${empl2[0]},${empl2[1]}?overview=simplified&geometries=polyline`, resp => {
+	  data = "";
+  
+	  resp.on("data", chunk => {
+		data += chunk
+	  });
+  
+	  resp.on("end", () => {
+		geometry = JSON.parse(data).routes[0].geometry;
+		res.json(polyline.decode(geometry));
+	  });
+	});
+  
+	//res.json({"nope": "nope"});
+  });
+  
+  function getCompleteRoute(L, startPoint, callback) {
+	L = [startPoint].concat(L);
+	points = "";
+	for (p of L) {
+	  points += p[0].toString() + "," + p[1].toString();
+	  points += ";"
+	}
+	points = points.substring(0, points.length-1);
+  
+	httpRequest(`http://router.project-osrm.org/trip/v1/foot/${points}?source=first&geometries=polyline`, data => {
+	  geometry = JSON.parse(data).trips[0].geometry;
+	  callback(polyline.decode(geometry));
+	});
+  }
+  
+  //getCompleteRoute([[2.1484799385071, 43.925579071045], [2.1436800956726, 43.929229736328]], [2.14513, 43.92274], res => console.log(res));
+  
+  server.listen(3001, () => {
   console.log("listening on *:3001");
 });
 
@@ -52,7 +135,9 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", (socket) => {		
 		players.delete(socket.id);
 		console.log("Someone left: " + (players.size).toString() + " player(s)");
-	})
-});
-
+	});
+	socket.on("Itineraire",(obj)=>{
+		console.log(obj);
+	});
+})
 // TODO: AJouter les joueurs dans un JSON pour les déconnections
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,18 @@ import Main from './Main'
 
 import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
 import {useState,useEffect} from 'react';
+import socketIOClient from "socket.io-client";
+import { initiateSocketConnection } from '../socketio.service.js';
 
 
 function App() {
   const [lang, setLang] = useState("en")
   
+  useEffect(() => {
+    initiateSocketConnection();
+  }, []);
+
+
   
     return (
       <div id='App' className="container-fluid ">
